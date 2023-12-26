dd3950a42a19875466f2c7b5bd4f57c7c4c5acf4
mingjianglong <l502858283@gmail.com>
2023-12-26 11:10:25 +0800

    feat: 增加导出

--- src/util/Check.ts
+++ src/util/Check.ts
@@ -16,6 +16,10 @@ export function isArray(value: any): value is Array<any> {
   return Array.isArray(value)
 }
 
+export function isNotArray(value: any): value is Exclude<typeof value, Array<any>> {
+  return !isArray(value)
+}
+
 export function isUndefined(value: any): value is undefined {
   return value === undefined
 }
