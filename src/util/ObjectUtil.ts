77bb9e004d7d169808f5de8f42eba182939f6c32
mingjianglong <l502858283@gmail.com>
2023-12-26 11:15:34 +0800

    feat: 更新入口

--- src/util/ObjectUtil.ts
+++ src/util/ObjectUtil.ts
@@ -2,7 +2,7 @@ type ObjectDes<T = any> = Record<string, T>
 /**
  * Object排除一些值,当predict为true
  */
-export function excludeItems(value: ObjectDes, predict: (value: any) => boolean) {
+function excludeItems(value: ObjectDes, predict: (value: any) => boolean) {
   return Object.keys(value).reduce((result, current) => {
     const temp = value[current];
     const predictResult = predict(temp);
@@ -13,11 +13,15 @@ export function excludeItems(value: ObjectDes, predict: (value: any) => boolean)
     }
   }, {})
 }
-export function pickItems<T = any>(value: ObjectDes, keys: string[]) {
+function pickItems<T = any>(value: ObjectDes, keys: string[]) {
   return Object.keys(keys).reduce((result, key) => {
     return {
       ...result,
       [key]: value[key]
     }
   }, {}) as T
+}
+
+export const ObjectUtil = {
+  excludeItems, pickItems
 }
